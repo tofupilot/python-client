c18a1775f9251656f9d85dd5bd7f80305bcabcc0
Felix <felix.berthier@epita.fr>
2024-06-07 19:44:41 +0200

    0.1.2

--- tofupilot/client.py
+++ tofupilot/client.py
@@ -1,7 +1,7 @@
 import requests
 import time
 from datetime import timedelta
-from typing import Callable
+from typing import Callable, Dict
 
 class TofuPilotClient:
     def __init__(self, api_key: str, base_url: str = "https://www.tofupilot.com/api/v1"):
@@ -12,9 +12,9 @@ class TofuPilotClient:
             "Authorization": f"Bearer {self.api_key}"
         }
 
-    def create_test_run(self, procedure_id: str, component_id: str, component_revision: str, unit_sn: str, function_to_measure: Callable[[], bool]) -> dict:
+    def create_test_run(self, procedure_id: str, component_id: str, component_revision: str, unit_sn: str, test_function: Callable[[], bool], params: Dict[str, str] = None) -> dict:
         start_time = time.time()
-        test_result = function_to_measure()
+        test_result = test_function()
         end_time = time.time()
         duration_seconds = end_time - start_time
 
@@ -22,13 +22,18 @@ class TofuPilotClient:
         duration = timedelta(seconds=duration_seconds)
         iso_duration = f"P{duration.days}DT{duration.seconds // 3600}H{(duration.seconds // 60) % 60}M{duration.seconds % 60}.{duration.microseconds}S"
 
+        # Set default params to an empty dictionary if None is provided
+        if params is None:
+            params = {}
+
         payload = {
             "procedure_id": procedure_id,
             "component_id": component_id,
             "component_revision": component_revision,
             "unit_sn": unit_sn,
             "run_passed": test_result,
-            "duration": iso_duration
+            "duration": iso_duration,
+            "params": params
         }
 
         response = requests.post(
