9ead372644718b6e1990293e6aa461ca13ff64c8
Felix <felix.berthier@epita.fr>
2024-06-10 15:43:16 +0200

    Made component_revision parameter of create_test_run method optional

--- tofupilot/client.py
+++ tofupilot/client.py
@@ -33,7 +33,7 @@ class TofuPilotClient:
         except ValueError:
             return f"HTTP error occurred: {response.text}"
 
-    def create_test_run(self, procedure_id: str, component_id: str, component_revision: str, unit_sn: str, test_function: Callable[[], bool], params: Dict[str, str] = None) -> dict:
+    def create_test_run(self, procedure_id: str, component_id: str, unit_sn: str, test_function: Callable[[], bool], component_revision: str = None, params: Dict[str, str] = None) -> dict:
         start_time = time.time()
         test_result = test_function()
         end_time = time.time()
@@ -43,19 +43,21 @@ class TofuPilotClient:
         duration = timedelta(seconds=duration_seconds)
         iso_duration = f"P{duration.days}DT{duration.seconds // 3600}H{(duration.seconds // 60) % 60}M{duration.seconds % 60}.{duration.microseconds}S"
 
-        # Set default params to an empty dictionary if None is provided
-        if params is None:
-            params = {}
-
         payload = {
             "procedure_id": procedure_id,
             "component_id": component_id,
-            "component_revision": component_revision,
             "unit_sn": unit_sn,
             "run_passed": test_result,
             "duration": iso_duration,
-            "params": params
         }
+
+        # We include component_revision if it is not None
+        if component_revision is not None:
+            payload["component_revision"] = component_revision
+
+        # We include params if it is not None
+        if params is not None:
+            payload["params"] = params
 
         try:
             response = requests.post(
