5f79a588da2d55966c123d1f1be33ea55e5d5b0e
Felix <felix.berthier@epita.fr>
2024-07-15 11:32:13 +0200

    Split source codes into different folders

---
+++ tofupilot/models/__init__.py
@@ -0,0 +1,7 @@
+from .unit_under_test import UnitUnderTest
+from .sub_unit import SubUnit
+
+__all__ = [
+    'UnitUnderTest',
+    'SubUnit'
+]
---
+++ tofupilot/models/sub_unit.py
@@ -0,0 +1,4 @@
+from typing import TypedDict
+
+class SubUnit(TypedDict):
+    serial_number: str
