7f34f0123a1013a2b54c4b962f1b359743297505
Felix <felix.berthier@epita.fr>
2024-07-17 10:43:10 +0200

    Revert "Moved source code to src directory"
    This reverts commit 472f08f981c84166840bcdc8f9a23cdb7159d9ce.

--- tofupilot/utils/version_checker.py
+++ tofupilot/utils/version_checker.py
@@ -1,14 +1,11 @@
 import warnings
-from importlib.metadata import version as get_version, PackageNotFoundError
 import requests
 from packaging import version
+from importlib.metadata import version as get_version, PackageNotFoundError
 
 def check_latest_version(logger, package_name: str):
-    """
-    Checks that package version is up to date, emits a warning otherwise
-    """
     try:
-        response = requests.get(f'https://pypi.org/pypi/{package_name}/json', timeout=10) # 10 seconds
+        response = requests.get(f'https://pypi.org/pypi/{package_name}/json')
         response.raise_for_status()
         latest_version = response.json()['info']['version']
         
