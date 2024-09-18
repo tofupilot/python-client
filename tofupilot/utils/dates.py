c04cf60b69f7481f9f9b4aa6f5437354b36ac98b
Felix <felix.berthier@epita.fr>
2024-09-18 11:09:57 +0200

    chore: split utils files

---
+++ tofupilot/utils/dates.py
@@ -0,0 +1,36 @@
+from datetime import timedelta, datetime
+
+
+def timedelta_to_iso(td: timedelta) -> str:
+    if td == timedelta():  # Check if timedelta is zero
+        return "PT0S"  # Return "PT0S" for zero duration
+
+    days = td.days
+    seconds = td.seconds
+    microseconds = td.microseconds
+
+    hours, remainder = divmod(seconds, 3600)
+    minutes, seconds = divmod(remainder, 60)
+
+    iso_duration = "P"
+    if days > 0:
+        iso_duration += f"{days}D"
+
+    if hours > 0 or minutes > 0 or seconds > 0 or microseconds > 0:
+        iso_duration += "T"
+        if hours > 0:
+            iso_duration += f"{hours}H"
+        if minutes > 0:
+            iso_duration += f"{minutes}M"
+        if seconds > 0 or microseconds > 0:
+            iso_duration += f"{seconds}"
+            if microseconds > 0:
+                iso_duration += f".{microseconds:06d}"
+            iso_duration += "S"
+
+    return iso_duration
+
+
+def datetime_to_iso(dt: datetime):
+    # Note: using dt.isoformat() does not add the trailing 'Z' which prevents the iso string from being recognized by TP' API
+    return dt.strftime("%Y-%m-%dT%H:%M:%S.%fZ")
