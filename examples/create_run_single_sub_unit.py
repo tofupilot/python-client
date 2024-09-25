1d3597e25a06c7c4c60550e45589600bddb5063f
Felix <felix.berthier@epita.fr>
2024-09-25 15:02:33 +0200

    Simplified example scripts

--- examples/create_run_single_sub_unit.py
+++ examples/create_run_single_sub_unit.py
@@ -3,17 +3,10 @@ from tofupilot import TofuPilotClient
 client = TofuPilotClient()
 
 
-def test_function():
-    # your test execution goes here
-    return True
-
-
-run_passed = test_function()
-
 response = client.create_run(
     procedure_id="FVT1",
     unit_under_test={"serial_number": "00002", "part_number": "SI001"},
-    run_passed=run_passed,
+    run_passed=True,
     sub_units=[{"serial_number": "00102"}],
 )
 
--- examples/create_run_with_attachments.py
+++ examples/create_run_with_attachments.py
@@ -3,24 +3,14 @@ from tofupilot import TofuPilotClient
 client = TofuPilotClient()
 
 
-def test_function():
-    # test status
-    run_passed = True
-    # local path to attachements
-    attachments = [
-        "data/temperature-map.png",  # path to your local files
-        "data/performance-report.pdf",
-    ]
-    return run_passed, attachments
-
-
-run_passed, attachments = test_function()
-
 response = client.create_run(
     procedure_id="FVT1",
     unit_under_test={"serial_number": "00102", "part_number": "PCB01"},
-    run_passed=run_passed,
-    attachments=attachments,
+    run_passed=True,
+    attachments=[
+        "data/temperature-map.png",  # path to your local files
+        "data/performance-report.pdf",
+    ],
 )
 
 success = response.get("success")
