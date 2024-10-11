e84cb77b47ddb81a544fcc5599da0b358a240e78
Felix <felix.berthier@epita.fr>
2024-10-11 19:14:51 +0200

    chore: 1.4.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as file:
 
 setup(
     name="tofupilot",
-    version="1.3.0",
+    version="1.4.0",
     packages=find_packages(),
     install_requires=[
         "requests",
