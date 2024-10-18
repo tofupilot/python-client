ede79473aadddaf42d28b602f8ab1a77f3177e25
Felix <felix.berthier@epita.fr>
2024-10-18 13:29:42 +0200

    1.5.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as file:
 
 setup(
     name="tofupilot",
-    version="1.5.0",
+    version="1.5.1",
     packages=find_packages(),
     install_requires=[
         "requests",
