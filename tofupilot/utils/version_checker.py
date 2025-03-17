c341e607e1da1997c80c4a2d1ff7b319a65ed2c7
AlexButeau <alexbuteau1@gmail.com>
2025-03-17 09:41:45 +0100

    chore: more precise check of python version - SDK-33

--- tofupilot/utils/version_checker.py
+++ tofupilot/utils/version_checker.py
@@ -20,11 +20,20 @@ def check_latest_version(logger, current_version, package_name: str):
         try:
             if version.parse(current_version) < version.parse(latest_version):
                 warning_message = (
-                    f"You are using {package_name} version {current_version}, however version {latest_version} is available. "
+                    f" You are using {package_name} version {current_version}, however version {latest_version} is available. "
                     f'You should consider upgrading via the "pip install --upgrade {package_name}" command.\n'
                     f"You may need to upgrade Python first. You current version of Python is {current_python_version} and the latest version of tofupilot needs at least {minimal_python_version}"
                 )
                 logger.warning(warning_message)
+            if sys.version_info.major == 2:
+                logger.error(
+                    f"ERROR: Python {current_python_version} is no longer supported. Please upgrade to Python {minimal_python_version} or later to use this client."
+                )
+            # all tofupilot versions need at least Python 3.6
+            elif version.parse(current_python_version) < version.parse("3.5"):
+                logger.warning(
+                    f" WARNING: Your Python version {current_python_version} is outdated. Some features may not work correctly. Consider upgrading to Python {minimal_python_version}."
+                )
         except PackageNotFoundError:
             logger.info(f"Package {package_name} is not installed.")
 
