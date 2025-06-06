1680a11837bca2bb1e3397b16cb64302e977562a
Quentin Bernet <quentin.bernet@bluewin.ch>
2025-06-06 11:17:19 +0200

    First working version

--- tofupilot/openhtf/operator_ui.py
+++ tofupilot/openhtf/operator_ui.py
@@ -1,11 +1,7 @@
+import openhtf
+from openhtf import plugs
 from openhtf.core.base_plugs import FrontendAwareBasePlug
 
-import functools
-import logging
-import os
-import platform
-import select
-import sys
 import threading
 from typing import Any, Callable, Dict, Optional, Union
 import uuid
@@ -13,50 +9,51 @@ from dataclasses import dataclass
 from abc import ABC, abstractmethod
 from typing import Union, Tuple
 
-import attr
-
 @dataclass(frozen=True)
 class Element(ABC):
     @abstractmethod
-    def as_dict(self):
+    def _asdict(self):
         pass
 
 @dataclass(frozen=True)
 class Text(Element):
     s: str
 
-    def as_dict(self):
+    def _asdict(self):
         return { "class": "Text", "s": self.s}
     
 @dataclass(frozen=True)
 class TextInput(Element):
     placeholder: Optional[str]
 
-    def as_dict(self):
+    def _asdict(self):
         return { "class": "TextInput", "s": self.placeholder}
 
 @dataclass(frozen=True)
 class Select(Element):
     choices: Tuple[str, ...]
 
-    def as_dict(self):
+    def _asdict(self):
         return { "class": "Select", "choices": self.choices}
 
 @dataclass(frozen=True)  
 class TopDown(Element):
     children: Tuple['Element', ...]
 
-    def as_dict(self):
-        print(self.children)
-        children_dicts = tuple(map(lambda c: c.as_dict(), self.children))
-        return { "class": "TopDown", "options": children_dicts}
+    def _asdict(self):
+        children_dicts = tuple(map(lambda c: c._asdict(), self.children))
+        return { "class": "TopDown", "children": children_dicts}
 
-@attr.s(slots=True, frozen=True)
-class Prompt(object):
-    id = attr.ib(type=Text)
-    message = attr.ib(type=Text)
-    text_input = attr.ib(type=bool)
-    image_url = attr.ib(type=Optional[Text], default=None)
+@dataclass(frozen=True)
+class Prompt:
+    id: str
+    element: Element
+
+    def _asdict(self) -> Dict[str, Any]:
+        return {
+            'id': self.id,
+            'element': self.element._asdict(),
+        }
 
 class OperatorUiPlug(FrontendAwareBasePlug):
     """Get user input from inside test phases.
@@ -68,6 +65,7 @@ class OperatorUiPlug(FrontendAwareBasePlug):
 
     def __init__(self):
         super(OperatorUiPlug, self).__init__()
+        # TODO: Remove last_response
         self.last_response: Optional[tuple[str, str]] = None
         self._prompt: Optional[Prompt] = None
         #self._console_prompt: Optional[ConsolePrompt] = None
@@ -79,12 +77,7 @@ class OperatorUiPlug(FrontendAwareBasePlug):
         with self._cond:
             if self._prompt is None:
                 return None
-            return {
-                'id': self._prompt.id,
-                'message': self._prompt.message,
-                'text-input': self._prompt.text_input,
-                'image-url': self._prompt.image_url
-            }
+            return self._prompt._asdict()
 
     def tearDown(self) -> None:
         self.remove_prompt()
@@ -98,50 +91,11 @@ class OperatorUiPlug(FrontendAwareBasePlug):
             #  self._console_prompt = None
             self.notify_update()
 
-    def prompt(self,
-                message: Text,
-                text_input: bool = False,
-                timeout_s: Union[int, float, None] = None,
-                cli_color: Text = '',
-                image_url: Optional[Text] = None) -> Text:
-        """Display a prompt and wait for a response.
-
-        Args:
-        message: A string to be presented to the user.
-        text_input: A boolean indicating whether the user must respond with text.
-        timeout_s: Seconds to wait before raising a PromptUnansweredError.
-        cli_color: An ANSI color code, or the empty string.
-        image_url: Optional image URL to display or None.
-
-        Returns:
-        A string response, or the empty string if text_input was False.
-
-        Raises:
-        MultiplePromptsError: There was already an existing prompt.
-        PromptUnansweredError: Timed out waiting for the user to respond.
-        """
-        self.start_prompt(message, text_input, cli_color, image_url)
+    def prompt(self, element: Element, *, timeout_s: Union[int, float, None] = None) -> str:
+        self.start_prompt(element)
         return self.wait_for_prompt(timeout_s)
 
-    def start_prompt(self,
-                    message: Text,
-                    text_input: bool = False,
-                    cli_color: Text = '',
-                    image_url: Optional[Text] = None) -> Text:
-        """Display a prompt.
-
-        Args:
-        message: A string to be presented to the user.
-        text_input: A boolean indicating whether the user must respond with text.
-        cli_color: An ANSI color code, or the empty string.
-        image_url: Optional image URL to display or None.
-
-        Raises:
-        MultiplePromptsError: There was already an existing prompt.
-
-        Returns:
-        A string uniquely identifying the prompt.
-        """
+    def start_prompt(self, element: Element) -> str:
         with self._cond:
             #if self._prompt:
             #  raise MultiplePromptsError(
@@ -150,35 +104,18 @@ class OperatorUiPlug(FrontendAwareBasePlug):
 
             self._response = None
             self._prompt = Prompt(
-                id=prompt_id,
-                message=message,
-                text_input=text_input,
-                image_url=image_url)
-            #if sys.stdin.isatty():
-                #self._console_prompt = ConsolePrompt(message, functools.partial(self.respond, prompt_id), cli_color)
-                #self._console_prompt.start()
+                prompt_id,
+                element
+            )
 
             self.notify_update()
             return prompt_id
 
     def wait_for_prompt(self, timeout_s: Union[int, float, None] = None) -> str:
-        """Wait for the user to respond to the current prompt.
-
-        Args:
-        timeout_s: Seconds to wait before raising a PromptUnansweredError.
-
-        Returns:
-        A string response, or the empty string if text_input was False.
-
-        Raises:
-        PromptUnansweredError: Timed out waiting for the user to respond.
-        """
         with self._cond:
             if self._prompt:
-                if timeout_s is None:
-                    self._cond.wait(3600 * 24 * 365)
-                else:
-                    self._cond.wait(timeout_s)
+                # if timeout_s is none, wait forever
+                self._cond.wait(timeout_s)
             #if self._response is None:
             #  raise PromptUnansweredError
             return self._response
@@ -191,7 +128,7 @@ class OperatorUiPlug(FrontendAwareBasePlug):
 
         Args:
         prompt_id: A string uniquely identifying the prompt.
-        response: A string response to the given prompt.
+        response: The response to the given prompt.
         """
         #_LOG.debug('Responding to prompt (%s): "%s"', prompt_id, response)
         with self._cond:
@@ -203,31 +140,6 @@ class OperatorUiPlug(FrontendAwareBasePlug):
             self._cond.notifyAll()
 
     # TODO: Reimplement this with new framework
-    """
-    def prompt_for_test_start(
-        message: str = 'Enter a DUT ID in order to start the test.',
-        timeout_s: Union[int, float, None] = 60 * 60 * 24,
-        validator: Callable[[str], str] = lambda sn: sn,
-        cli_color: str = '') -> openhtf.PhaseDescriptor:
-        ""Returns an OpenHTF phase for use as a prompt-based start trigger.
-
-        Args:
-            message: The message to display to the user.
-            timeout_s: Seconds to wait before raising a PromptUnansweredError.
-            validator: Function used to validate or modify the serial number.
-            cli_color: An ANSI color code, or the empty string.
-        ""
-
-        @openhtf.PhaseOptions(timeout_s=timeout_s)
-        @plugs.plug(prompts=UserInput)
-        def trigger_phase(test: openhtf.TestApi, prompts: UserInput) -> None:
-            ""Test start trigger that prompts the user for a DUT ID.""
-            dut_id = prompts.prompt(
-                message, text_input=True, timeout_s=timeout_s, cli_color=cli_color)
-            test.test_record.dut_id = validator(dut_id)
-
-        return trigger_phase
-    """
 
 class OperatorUi:
     plug = OperatorUiPlug
@@ -243,6 +155,45 @@ class OperatorUi:
     def select(*choices: str) -> Select:
         return Select(choices)
     
-    def top_down(*children: Union[str, Element]) -> TopDown:
-        elements: tuple[Element] = tuple(map(lambda c: Text(c) if isinstance(c, str) else c, children))
+    def top_down(*children: Union[str, Element, None]) -> TopDown:
+        
+        # Remove `None`s and convert `str` to `Text` elements
+        elements: tuple[Element] = tuple(
+            map(
+                lambda c: Text(c) if isinstance(c, str) else c,
+                filter(lambda c: c != None, children)
+            )
+        )
         return TopDown(elements)
+    
+def prompt_for_test_start(
+    message: str = 'Enter a DUT ID in order to start the test.',
+    timeout_s: Union[int, float, None] = 60 * 60 * 24,
+    validator: Callable[[str], str] = lambda sn: sn,
+    ) -> openhtf.PhaseDescriptor:
+    """Returns an OpenHTF phase for use as a prompt-based start trigger.
+
+    Drop-in replacement for openhtf.plugs.user_input.prompt_for_test_start.
+    (If you were using cli_color, remove that parameter)
+
+    Args:
+        message: The message to display to the user.
+        timeout_s: Seconds to wait before raising a PromptUnansweredError.
+        validator: Function used to validate or modify the serial number.
+    """
+    ui = OperatorUi
+
+    @openhtf.PhaseOptions(timeout_s=timeout_s)
+    @plugs.plug(ui_plug=OperatorUi.plug)
+    def trigger_phase(test: openhtf.TestApi, ui_plug: OperatorUi.plug) -> None:
+        """Test start trigger that prompts the user for a DUT ID."""
+        dut_id = ui_plug.prompt(
+            ui.top_down(
+                message,
+                ui.text_input(),
+            ),
+            timeout_s=timeout_s,
+        )
+        test.test_record.dut_id = validator(dut_id)
+
+    return trigger_phase
